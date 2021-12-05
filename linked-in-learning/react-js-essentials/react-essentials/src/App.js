2049baffb3bb3f920a6b4eb70f62916f9c7fe3b9
Saru Thuraiman <sthuraisingh@bloomberg.net>
2021-12-05 00:30:13 -0500

    Fleshing out app for Saru's கையேந்தி பவன் 🤤

--- linked-in-learning/react-js-essentials/react-essentials/src/App.js
+++ linked-in-learning/react-js-essentials/react-essentials/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <h1>Saru's கையேந்தி பவன் 🤤</h1>
+    </header>
+  )
+}
+
+function Main() {
+  return (
+    <section>
+      <p> Deliciious street food on the road 🤤</p>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>மெய்யாலுமே!</p>
+    </footer>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Header</h1>
-      <h2>Main</h2>
-      <h3>Footer</h3>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
